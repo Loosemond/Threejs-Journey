5499e46c2a1ca4390aeb2aa0a4c256e0bb736443
Loosemond <joao--ribeiro@hotmail.com>
2022-09-10 20:51:50 +0100

    Lesson 5

--- 05-transform-objects/src/script.js
+++ 05-transform-objects/src/script.js
@@ -1,5 +1,6 @@
 import './style.css'
 import * as THREE from 'three'
+import { WireframeGeometry } from 'three'
 
 // Canvas
 const canvas = document.querySelector('canvas.webgl')
@@ -10,10 +11,51 @@ const scene = new THREE.Scene()
 /**
  * Objects
  */
-const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
-const mesh = new THREE.Mesh(geometry, material)
-scene.add(mesh)
+const group = new THREE.Group()
+
+const mesh_1 = new THREE.Mesh(new THREE.BoxGeometry(1, 1, 1), new THREE.MeshBasicMaterial({ color: "#F06060", wireframe: true, }))
+const mesh_2 = new THREE.Mesh(new THREE.BoxGeometry(1, 1, 1), new THREE.MeshBasicMaterial({ color: "#F3B562", wireframe: true, transparent: true, opacity: 1 }))
+const mesh_3 = new THREE.Mesh(new THREE.BoxGeometry(1, 1, 1), new THREE.MeshBasicMaterial({ color: "#8CBEB2", wireframe: true }))
+
+
+mesh_1.position.set(3, 0, 0)
+mesh_2.position.set(0, -3, 0)
+
+// mesh_1.scale.set(0.5, 1, 1)
+// mesh_2.scale.set(0.5, 1, 1)
+// mesh_3.scale.set(0.5, 1, 1)
+group.add(mesh_1)
+group.add(mesh_2)
+group.add(mesh_3)
+
+
+
+
+// X,Y,Z
+group.position.set(1, 2.5, 0)
+
+// Limits the distange to origin to one
+group.position.normalize()
+
+scene.add(group)
+console.log()
+
+// Axes helper Shows the axis on origin
+// Red X , Green Y, Blue Z
+const axesHelper = new THREE.AxesHelper(2)
+scene.add(axesHelper)
+
+// Scale
+// Also scales the space between the cubes
+group.scale.set(1, 0.5, 1.2)
+
+// Rotation
+group.rotation.y = Math.PI / 4
+group.rotation.z = Math.PI / 4
+group.rotation.reorder("XYZ")
+
+
+
 
 /**
  * Sizes
@@ -26,9 +68,19 @@ const sizes = {
 /**
  * Camera
  */
+
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
 camera.position.z = 3
+camera.position.x = .2
+
 scene.add(camera)
+// it will orent the object to look at another object or a vector
+camera.lookAt(group.position)
+
+
+// Distance to camera
+console.log(group.position.distanceTo(camera.position))
+
 
 /**
  * Renderer
